1bf198af48065ccb559a467458926f2fd7c378c4
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-16 02:44:30 +0530

    React integration using Webpack and Babel

--- api/views.py
+++ api/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def main(request):
-    return HttpResponse("Hello World!")
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
+
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
